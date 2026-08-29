a80194266420d99add4d1bab488d1b95881d25c8
Awadhesh123-1 <awadheshrawat26@gmail.com>
2026-08-29 20:23:32 +0530

    Create transaction.py

---
+++ transaction.py
@@ -0,0 +1,23 @@
+import time
+
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.timestamp = time.time()
+
+    def to_dict(self):
+        return {
+            "sender": self.sender,
+            "receiver": self.receiver,
+            "amount": self.amount,
+            "timestamp": self.timestamp
+        }
+
+    def __str__(self):
+        return (
+            f"{self.sender} -> {self.receiver}: "
+            f"{self.amount}"
+        )

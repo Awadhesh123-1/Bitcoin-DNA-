3b21436a923f384eb84095c78e5950ccf7ec75b7
Awadhesh Rawat <abc@gmail.com>
2026-09-17 20:02:58 +0530

    Bitcoin-DNA Live Server Mainnet Upgrade

--- main.py
+++ main.py
@@ -24,5 +24,7 @@ def submit_work():
 
 if __name__ == '__main__':
     # Server ko Hotspot network par live karna (Port 8080)
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 

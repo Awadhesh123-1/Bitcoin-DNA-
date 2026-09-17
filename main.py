7886c50814a8dcb7a50fb7ba87d3082ee088b086
Awadhesh Rawat <abc@gmail.com>
2026-09-17 20:12:00 +0530

    Updated dynamic application server port to 5000

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def submit_work():
 if __name__ == '__main__':
     # Server ko Hotspot network par live karna (Port 8080)
     import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
